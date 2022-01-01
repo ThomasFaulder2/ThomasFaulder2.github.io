7762fb6aa320d690bf5582494b11addd686f1ba6
ThomasFaulder2 <75229419+ThomasFaulder2@users.noreply.github.com>
2022-01-01 19:43:35 +1100

    Update epic_cards.php

--- epic_cards.php
+++ epic_cards.php
@@ -1,14 +1,6 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "password";
-	$dbName = "myDB";
-	$conn = new mysqli($servername, $username, "", "epic_cards");
-	$sql = "INSERT INTO epic_cards (history, examination, investigations, management, diagnosis, contibutor, likes, dislikes, dateSub) VALUES ('John', 'HR 150', 'ECG', 'GTN', 'MI', 'T F', 100, 14, '1/1/21')";
-	if ($conn->query($sql) === TRUE) {
-		echo "Success!";
-	}
+
 	mail("tomfaulder@gmail.com", "subject", "new submission");
-	$conn->close();
+	
 
 ?>
